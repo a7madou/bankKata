b77a03fb32a3c33eafc9e4e048ac09272b19b562
ahmed.benmessaoud <benmassoued.ahmed@gmail.com>
2019-12-09 02:22:42 +0100

    Correction faute d'orthographe

--- src/main/java/org/sg/entretien/constant/TransactionConstant.java
+++ src/main/java/org/sg/entretien/constant/TransactionConstant.java
@@ -2,7 +2,7 @@ package org.sg.entretien.constant;
 
 public final class TransactionConstant {
 
-    public static final String INSUFFICIENT_ERROR_MESSAGE = "the balance in the Account be insufficient";
+    public static final String INSUFFICIENT_ERROR_MESSAGE = "the balance in the Account is insufficient";
     public static final String NEGATIVE_AMOUNT_ERROR_MESSAGE = "the amount can not be negative";
 
 
